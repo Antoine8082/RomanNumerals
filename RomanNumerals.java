b243ea8d85d6a172d1688dd118d8bcd305364153
Antoine C <antoine.cesbron.perso@gmail.com>
2023-01-25 14:34:24 +0100

    Add Input too high/low code

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -1,6 +1,11 @@
 public class RomanNumerals {
 
     public static String convert(int num) {
+        if (num < 1 || num > 5000) {
+            String errorType = num < 1 ? "bas" : "haut";
+            throw new Error("Valeur incorrecte.(Trop " + errorType + ")");
+        }
+
         StringBuilder result = new StringBuilder();
         int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
         String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
