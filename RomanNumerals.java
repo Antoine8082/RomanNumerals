684afe97f5e288db910f602e9639c04a66aafcba
Antoine C <antoine.cesbron.perso@gmail.com>
2023-01-25 14:25:28 +0100

    Add Convert code

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -1,5 +1,15 @@
 public class RomanNumerals {
     public static String convert(int num) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        for (int i = 0; i < values.length; i++) {
+            while (values[i] <= num) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return result.toString();
     }
 }
